9c9b04bce112e0bd7f05b1282638fff464d998da
kadamKajal <156308375+kadamKajal@users.noreply.github.com>
2024-01-11 16:55:15 +0530

    Add files via upload

---
+++ Page1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Registration_Form
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
